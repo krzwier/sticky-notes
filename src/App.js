eec670ca0b42d6fd1f818a0885941f045812a5a6
krzwier <krzwier@gmail.com>
2021-06-05 08:11:36 -0500

    Transforms App into stateful component with data model

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
+import React, { Component } from "react";
 import './App.css';
 import Header from './Header.js';
 import NotesList from './NotesList';
 
-function App() {
-    return (
-        <div className="App">
-            <Header/>
-            <NotesList/>
-        </div>
-    );
+class App extends Component {
+
+    state = {
+        notes: [
+            {
+                id: 0,
+                title: "eat",
+                description: "reese peanut butter cups",
+                doesMatchSearch: true
+            },
+            {
+                id: 1,
+                title: "sleep",
+                description: "eight hours",
+                doesMatchSearch: true
+            },
+            {
+                id: 2,
+                title: "code",
+                description: "build an awesome ui",
+                doesMatchSearch: true
+            }
+        ],
+        searchText: ""
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <NotesList />
+            </div>
+        );
+    }
+    
 }
 
-export default App;
+    export default App;
